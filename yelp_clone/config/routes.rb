aa7dfa7cfad28a7c526797216bf43d200da8797c
cyberplanner <saleh.gfx@gmail.com>
2016-08-23 11:18:21 +0100

    Step 8, Creating restaurants.

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-resources :restaurants
 
 get 'restaurants' => 'restaurants#index'
+resources :restaurants
 
 end
   # Example of regular route:
