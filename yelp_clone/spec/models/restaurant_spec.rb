f4bf31d63f19b65cd1bbe80eaf7245da031b6b69
cyberplanner <saleh.gfx@gmail.com>
2016-08-23 16:58:32 +0100

    :tada: Version 1 Complete.

--- yelp_clone/spec/models/restaurant_spec.rb
+++ yelp_clone/spec/models/restaurant_spec.rb
@@ -5,4 +5,10 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Moe's Tavren")
+    restaurant = Restaurant.new(name: "Moe's Tavren")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
