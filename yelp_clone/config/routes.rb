81edda90c77c424aa32f8d6888a50d65d586b259
cyberplanner <saleh.gfx@gmail.com>
2016-08-25 11:18:23 +0100

    Removes a whitespace.

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -14,7 +14,6 @@ get 'restaurants' => 'restaurants#index'
   resources :restaurants do
     resources :reviews
   end
-
 end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
